f40bdb2ff9d97733890a73c1dcab18643d5c6147
Rajat Jindal <rajatjindal83@gmail.com>
2025-02-03 22:06:03 +0530

    update cool-sdk to use sdk source
    Signed-off-by: Rajat Jindal <rajatjindal83@gmail.com>

--- cool-sdk/dagger/main.go
+++ cool-sdk/dagger/main.go
@@ -5,11 +5,11 @@ import "main/internal/dagger"
 type CoolSdk struct{}
 
 func (m *CoolSdk) ModuleRuntime(modSource *dagger.ModuleSource, introspectionJson *dagger.File) *dagger.Container {
-	return modSource.WithSDK("go").AsModule().Runtime().WithEnvVariable("COOL", "true")
+	return modSource.WithSDK(dagger.ModuleSourceWithSDKOpts{Source: "go"}).AsModule().Runtime().WithEnvVariable("COOL", "true")
 }
 
 func (m *CoolSdk) Codegen(modSource *dagger.ModuleSource, introspectionJson *dagger.File) *dagger.GeneratedCode {
-	return dag.GeneratedCode(modSource.WithSDK("go").AsModule().GeneratedContextDirectory())
+	return dag.GeneratedCode(modSource.WithSDK(dagger.ModuleSourceWithSDKOpts{Source: "go"}).AsModule().GeneratedContextDirectory())
 }
 
 func (m *CoolSdk) RequiredPaths() []string {
