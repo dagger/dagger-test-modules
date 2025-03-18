7fe8377e7eace2d96978e655065e50d924953706
Connor Braa <connor@dagger.io>
2025-03-18 10:49:26 -0700

    add module that uses LLM api
    Signed-off-by: Connor Braa <connor@dagger.io>

---
+++ llm-test-module/main.go
@@ -0,0 +1,23 @@
+// A generated module for Llm functions
+// this module is necessary for testing allow-llm policy enforcement around git-sourced modules
+package main
+
+import (
+	"context"
+	"dagger/llm/internal/dagger"
+)
+
+type LlmTestModule struct {
+	Model string // +private
+}
+
+func New(
+	model string, // +optional
+) *LlmTestModule {
+	return &LlmTestModule{model}
+}
+
+// access the LLM api in the most minimal way: prompt it, then retrieve history
+func (m *LlmTestModule) Prompt(ctx context.Context, stringArg string) (string, error) {
+	return dag.Llm(dagger.LlmOpts{Model: m.Model}).WithPrompt(stringArg).LastReply(ctx)
+}
