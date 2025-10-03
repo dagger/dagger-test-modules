2182e126e3631605fd9894e130d1287f3699cbff
Yves Brissaud <yves.brissaud@gmail.com>
2025-10-03 16:10:12 +0200

    Update cool-sdk to follow moduleTypeDefs branch
    Signed-off-by: Yves Brissaud <yves.brissaud@gmail.com>

--- cool-sdk/dagger/main.go
+++ cool-sdk/dagger/main.go
@@ -2,14 +2,29 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 
 	"main/internal/dagger"
 )
 
 type CoolSdk struct{}
 
-func (m *CoolSdk) ModuleDefs(ctx context.Context, modSource *dagger.ModuleSource, introspectionJSON *dagger.File) (*dagger.Module, error) {
-	return modSource.WithSDK("go").AsModule(), nil
+func (m *CoolSdk) ModuleDefs(ctx context.Context, modSource *dagger.ModuleSource, introspectionJSON *dagger.File, outputFilePath string) (*dagger.Container, error) {
+	mod := modSource.WithSDK("go").AsModule()
+	modID, err := mod.ID(ctx)
+	if err != nil {
+		return nil, err
+	}
+	b, err := json.Marshal(modID)
+	if err != nil {
+		return nil, err
+	}
+	return dag.Container().
+		From("alpine").
+		WithNewFile(outputFilePath, string(b)).
+		WithEntrypoint([]string{
+			"sh", "-c", "",
+		}), nil
 }
 
 func (m *CoolSdk) ModuleRuntime(modSource *dagger.ModuleSource, introspectionJson *dagger.File) *dagger.Container {
